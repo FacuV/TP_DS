74b269d26ce5936a0ab658063265b1cbc0f584b1
Francisco Giancarelli <franciscogiancarelli@Franciscos-MBP.fibertel.com.ar>
2020-10-05 21:33:08 -0300

    Implementación basica del gestor de pantallas

---
+++ src/main/java/GestorPantallas/Pantalla.java
@@ -0,0 +1,13 @@
+package GestorPantallas;
+
+import javax.swing.*;
+
+public class Pantalla {
+    String nombre;
+    JFrame componente;
+
+    public Pantalla (String nombre, JFrame componente) {
+        this.nombre = nombre;
+        this.componente = componente;
+    }
+}
--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -1,14 +1,17 @@
 package Main;
 
+import GestorPantallas.GestorPantallas;
+import GestorPantallas.Pantalla;
 import Interface.Home;
 
 import javax.swing.*;
 import java.sql.SQLException;
 
 public class Main {
+
     public static void main(String[] args) throws SQLException {
         JFrame home = new Home();
-        home.setVisible(true);
+        GestorPantallas.add(new Pantalla("home",home));
+        GestorPantallas.init("home");
     }
-
 }
