824c4ea51cf0b692e3f0c32b1f7ca56d633ae86c
unknown <Facundo Velazco>
2020-10-07 17:29:46 -0300

    implementando hibernate y algunos daos

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -3,25 +3,46 @@ package Main;
 import GestorPantallas.GestorPantallas;
 import GestorPantallas.Pantalla;
 import Interface.JPantalla;
+import Negocio.Pais;
 import Pantallas.Home;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 import javax.swing.*;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
-        JFrame home = new Home();
-        JFrame pantalla = new JPantalla("una pantalla","tu vijea");
-        GestorPantallas.add(new Pantalla("home",home));
-        GestorPantallas.add(new Pantalla("pantalla",pantalla));
-        GestorPantallas.init("home");
+        //JFrame home = new Home();
+        //JFrame pantalla = new JPantalla("una pantalla","tu vijea");
+        //GestorPantallas.add(new Pantalla("home",home));
+        //GestorPantallas.add(new Pantalla("pantalla",pantalla));
+        //GestorPantallas.init("home");
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("Persistence");
+        EntityManager manager = entityManagerFactory.createEntityManager();
+        Pais australia= new Pais("Australia");
+        Pais hungria= new Pais("Hungria");
+        manager.getTransaction().begin();
+        //almacenar objeto
+        manager.persist(australia);
+        manager.persist(hungria);
+        manager.getTransaction().commit();
 
-        Class claseFrame = Class.forName("javax.swing.JFrame");
-        System.out.println(claseFrame.getConstructor());
-        Constructor constructor = claseFrame.getConstructor();
-        JFrame frame;
-        frame = (JFrame) constructor.newInstance();
-        frame.setVisible(true);
-        System.out.println(frame);
+        //manager.getTransaction().begin();
+        ////almacenar objeto
+        //australia=manager.find(Pais.class,0);
+        //hungria=manager.find(Pais.class,35);
+        //manager.remove(australia);
+        //manager.remove(hungria);
+        //manager.getTransaction().commit();
+        List paises = manager.createQuery("FROM Pais").getResultList();
+        System.out.println(paises);
+        System.out.println(paises.size());
+        manager.close();
+
+
     }
 }
--- src/main/java/Negocio/Pais.java
+++ src/main/java/Negocio/Pais.java
@@ -1,10 +1,39 @@
 package Negocio;
 
-public class Pais {
-    private int id_pais;
-    private String nombre;
+import javax.persistence.*;
 
+@Entity
+@Table(name = "pais")
+public class Pais{
+    @Id
+    @Column(name = "id_pais")
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private int id_pais;
+    @Column(name = "nombre")
+    private String nombre;
+    public Pais(){}
     public Pais(String nombre) {
         this.nombre = nombre;
+    }
+
+    public int getId_pais() {
+        return id_pais;
+    }
+
+    public void setId_pais(int id_pais) {
+        this.id_pais = id_pais;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public String toString() {
+        return nombre;
     }
 }
