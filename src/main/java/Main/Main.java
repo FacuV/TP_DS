c8589a32db123d53e9a43f71c17277bea000f3d2
unknown <Facundo Velazco>
2020-10-12 22:29:01 -0300

    aux

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -18,64 +18,15 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
-        //JFrame home = new Home();
-        //JFrame pantalla = new JPantalla("una pantalla","tu vijea");
-        //GestorPantallas.add(new Pantalla("home",home));
-        //GestorPantallas.add(new Pantalla("pantalla",pantalla));
-        //GestorPantallas.init("home");
         EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("Persistence");
         EntityManager manager = entityManagerFactory.createEntityManager();
         manager.getTransaction().begin();
-        Usuario usuario = new Usuario();
-        manager.persist(usuario);
-        usuario = manager.find(Usuario.class,1);
-        usuario.setNombre("Pablo");
-        usuario.setApellido("Leonarduzzi");
-        usuario.setNumero_documento("36001639");
-        usuario.setTipoDeDocumento(TipoDeDocumento.DNI);
-        Provincia provincia = manager.find(Provincia.class,2);
-        System.out.println(provincia);
-        Localidad localidad = new Localidad("Santa fe",provincia);
-        manager.persist(localidad);
-        System.out.println(localidad);
-        manager.persist(localidad);
-        //localidad = manager.find(Localidad.class,localidad);
-        //System.out.println(localidad);
-        usuario.setLocalidad(localidad);
-        Deporte deporte = manager.find(Deporte.class,1);
-        ArrayList<Deporte> deportes = new ArrayList(); deportes.add(deporte);
-        LugarRealizacion lugarRealizacion = new LugarRealizacion("La canchita",usuario,deportes,"el descampado");
-        manager.persist(lugarRealizacion);
-        lugarRealizacion = manager.merge(lugarRealizacion);
-        ArrayList<LugarRealizacion> lugares = new ArrayList<>(); lugares.add(lugarRealizacion);
-        usuario.setLugaresRealizacion(lugares);
-        Competencia competencia = new EliminatoriaSimple();
-        competencia.setNombre("competencia de pablo");
-        competencia.setReglamento("se juega con los pies");
-        Puntuacion puntuacion = new PuntuacionResultadoFinal();
-        manager.persist(puntuacion);
-        puntuacion = manager.merge(puntuacion);
-        competencia.setPuntuacion(puntuacion);
-        competencia.setEstado(Estado.CREADA);
-        competencia.setUsuario(usuario);
-        competencia.setDeporte(deporte);
-        Disponibilidad disponibilidad = new Disponibilidad(10,competencia,lugarRealizacion);
-        manager.persist(disponibilidad);
-        disponibilidad = manager.merge(disponibilidad);
-        ArrayList<Disponibilidad> disponibilidades = new ArrayList<>();
-        disponibilidades.add(disponibilidad);
-        competencia.setDisponibilidades(disponibilidades);
-        Participante participante = new Individuo("pablito","pablodl77@gmail.com");
-        manager.persist(participante);
-        participante = manager.merge(participante);
-        ArrayList<Participante> participantes = new ArrayList<>();
-        participantes.add(participante);
-        competencia.setParticipantes(participantes);
-        manager.persist(competencia);
-        competencia = manager.find(Competencia.class,1);
+        Competencia competencia = manager.find(Competencia.class,3);
+        System.out.println(competencia.getClass());
+        if(competencia.getClass() == Liga.class){
+            System.out.println(((Liga) competencia).getPuntos_empate());
+        }
         manager.getTransaction().commit();
-        System.out.println(competencia);
-        manager.close();
 
 
 
