3540b3fd921df31daa76a6e1f92c96954a9d7d41
FacuV <facundovelazco13@gmail.com>
2020-10-13 18:36:47 -0300

    integrando feature/daos a rama develop

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -1,16 +1,9 @@
 package Main;
 
-
-import Negocio.*;
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
 import java.lang.reflect.InvocationTargetException;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Main {
-    public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
+    public static void main(String[] args){
 
     }
 }
