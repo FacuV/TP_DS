410e9f2a4f15660f05f92e6192514d1df7d9672e
unknown <Facundo Velazco>
2020-10-16 18:06:43 -0300

    commit auxiliar

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -1,10 +1,17 @@
 package Main;
 
+import Daos.DeporteDao;
+import Daos.DeportePostgreSQLDao;
 import GestorPantallas.GestorPantallas;
 import GestorPantallas.Pantalla;
+import Negocio.Deporte;
 import Pantallas.MisCompetencias;
 import Pantallas.Home;
 import Pantallas.NuevaCompetencia;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 public class Main {
     public static void main(String[] args) {
@@ -12,5 +19,14 @@ public class Main {
         GestorPantallas.add(new Pantalla("competencias", MisCompetencias.class));
         GestorPantallas.add(new Pantalla("nueva_competencia", NuevaCompetencia.class));
         GestorPantallas.init("home");
+       //EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("Persistence");
+       //EntityManager manager;
+       //    manager = entityManagerFactory.createEntityManager();
+       //    Deporte deporte = new Deporte("League of Legends");
+       //    manager.getTransaction().begin();
+       //    manager.persist(deporte);
+       //    manager.getTransaction().commit();
+       //    manager.close();
+
     }
 }
