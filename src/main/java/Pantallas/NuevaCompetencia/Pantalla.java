f90ca4515197c7d99ca88ab23a9c11bb322071db
unknown <Facundo Velazco>
2020-10-18 12:22:39 -0300

    commit auxiliar

--- src/main/java/Pantallas/NuevaCompetencia/Pantalla.java
+++ src/main/java/Pantallas/NuevaCompetencia/Pantalla.java
@@ -13,7 +13,7 @@ import java.util.ArrayList;
 
 public class Pantalla extends JPantalla {
     public Pantalla() {
-        super("Nueva competencias","Volver");
+        super("Nueva competencia","Volver");
         body.setLayout(new GridBagLayout());
         body.add(new Cabecera(),new Cabecera.Constraints());
         body.add(new Cuerpo(), new Cuerpo.Constraints());
