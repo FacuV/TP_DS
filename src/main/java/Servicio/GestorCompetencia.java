999ad2a341c981ca0f397cd9606274e31390cdb1
FacuV <facundovelazco13@gmail.com>
2020-11-24 15:55:06 -0300

    Metodos de validación de crearComp() encapsulados

--- src/main/java/Servicio/GestorCompetencia.java
+++ src/main/java/Servicio/GestorCompetencia.java
@@ -10,25 +10,21 @@ public abstract class GestorCompetencia {
     private static LugarRealizacionDao lugarRealizacionDao = new LugarRealizacionPostgreSQLDao();
     private static UsuarioDao usuarioDao = new UsuarioPostgreSQLDao();
     private static DeporteDao deporteDao = new DeportePostgreSQLDao();
+    public static final int LIGA = 0;
+    public static final int ELIMINATORIA_SIMPLE = 1;
+    public static final int ELIMINATORIA_DOBLE = 2;
+    public static final int PUNTAJE = 0;
+    public static final int SETS = 1;
+    public static final int FINAL = 2;
 
     public static ArrayList<String> crearComp(CompetenciaDTO competenciaDTO){
         ArrayList<String> errores = new ArrayList<>(4);
-        final int LIGA = 0;
-        final int ELIMINATORIA_SIMPLE = 1;
-        final int ELIMINATORIA_DOBLE = 2;
-        final int PUNTAJE = 0;
-        final int SETS = 1;
-        final int FINAL = 2;
 
         //validaciones logicas
-        if(competenciaDao.nombreUnico(competenciaDTO.nombre)){errores.add("El nombre ingresado ya existe en otra competencia");
-        }else{errores.add(null);}
-        if(competenciaDTO.forma_Puntuación == SETS &&(competenciaDTO.cantidad_máxima_sets % 2 == 0) || competenciaDTO.cantidad_máxima_sets > 10){errores.add("Cantidad sets par o mayor a 10");
-        }else{errores.add(null);}
-        if(competenciaDTO.modalidad_competencia == LIGA && competenciaDTO.puntos_partido_ganado < competenciaDTO.puntos_partido_empatado){errores.add("Puntos por partido ganado menor que puntos partido empatado");
-        }else{errores.add(null);}
-        if(competenciaDTO.modalidad_competencia == LIGA && competenciaDTO.puntos_presentarse >= competenciaDTO.puntos_partido_ganado){errores.add("Puntos por presentarse mayor o igual a puntos por partido ganado");
-        }else{errores.add(null);}
+        errores.add(nombreValido(competenciaDTO));
+        errores.add(setsValidos(competenciaDTO));
+        errores.add(puntuacionEmpateValido(competenciaDTO));
+        errores.add(puntuacionPresentarseValido(competenciaDTO));
         //retorno de errores
         for(String error:errores){if(error != null)return errores;}
         //Puntaje
@@ -58,5 +54,23 @@ public abstract class GestorCompetencia {
         competencia.setDisponibilidades(disponibilidades);
         competenciaDao.createCompetencia(competencia);
         return errores;
+    }
+
+    private static String nombreValido(CompetenciaDTO competenciaDTO){
+        System.out.println("hola");
+        if(competenciaDao.nombreUnico(competenciaDTO.nombre)){return "El nombre ingresado ya existe en otra competencia";}
+        else{return null;}
+    }
+    private static String setsValidos(CompetenciaDTO competenciaDTO){
+        if(competenciaDTO.forma_Puntuación == SETS &&(competenciaDTO.cantidad_máxima_sets % 2 == 0) || competenciaDTO.cantidad_máxima_sets > 10){return "Cantidad sets par o mayor a 10";}
+        else{return null;}
+    }
+    private static String puntuacionEmpateValido(CompetenciaDTO competenciaDTO){
+        if(competenciaDTO.modalidad_competencia == LIGA && competenciaDTO.puntos_partido_ganado < competenciaDTO.puntos_partido_empatado){return "Puntos por partido ganado menor que puntos partido empatado";}
+        else{return null;}
+    }
+    private static String puntuacionPresentarseValido(CompetenciaDTO competenciaDTO){
+        if(competenciaDTO.modalidad_competencia == LIGA && competenciaDTO.puntos_presentarse >= competenciaDTO.puntos_partido_ganado){return "Puntos por presentarse mayor o igual a puntos por partido ganado";}
+        else{return null;}
     }
 }
